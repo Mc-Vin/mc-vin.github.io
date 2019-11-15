9039574b5ff369eca596855fced366f5f37c0dee
Mc-Vin <taozhudut@hotmail.com>
2019-11-15 11:20:55 +1100

    'index'

---
+++ precache-manifest.5840d5ae946ef1079fd7f7388ae17160.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78f1ad54636bf15fb470aaf4f9307a5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e623e75fece7e721ce67",
+    "url": "/static/css/main.a066a666.chunk.css"
+  },
+  {
+    "revision": "e422ef2c884406397cd7",
+    "url": "/static/js/2.8e6472c9.chunk.js"
+  },
+  {
+    "revision": "e623e75fece7e721ce67",
+    "url": "/static/js/main.5d83b4b1.chunk.js"
+  },
+  {
+    "revision": "f885ebc6c45987211826",
+    "url": "/static/js/runtime-main.c18a0d0f.js"
+  },
+  {
+    "revision": "c2a81a2250ded5e19a7681ff525eb33d",
+    "url": "/static/media/account-head.c2a81a22.png"
+  }
+]);
